584fc8fcc403a0af6969e63842f4687dd1c00907
spmarks <spencer.marks@cloudlock.com>
2017-09-18 08:02:17 -0400

    remove non puzzle coed

--- sorters/src/main/java/com/orgamisoftware/puzzles/sorters/MergeSort.java
+++
@@ -1,9 +0,0 @@
-package com.orgamisoftware.puzzles.sorters;
-
-class MergeSort implements Sorter {
-
-    @Override
-    public int[] sort(int[] ints) {
-        return new int[0];
-    }
-}
--- sorters/src/main/java/com/orgamisoftware/puzzles/sorters/SorterType.java
+++
@@ -1,6 +0,0 @@
-package com.orgamisoftware.puzzles.sorters;
-
-public enum SorterType {
-
-    QUICK_SORT, MERGE_SORT;
-}
